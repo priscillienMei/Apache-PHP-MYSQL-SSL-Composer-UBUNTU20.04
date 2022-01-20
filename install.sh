c712692dd708bfeb6ea3a7e431cedad69ed253fe
priscillienMei <86047095+priscillienMei@users.noreply.github.com>
2022-01-20 14:24:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo a2enmod actions fcgid alias proxy_fcgi ssl rewrite
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 HASH=`curl -sS https://composer.github.io/installer.sig`
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php');  exit; } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 echo "<VirtualHost *:80>
